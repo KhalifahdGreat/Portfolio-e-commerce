92d6c9811e43ab71023a19dde8978bb249beaaa9
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-11-17 16:42:29 +0100

    reverse-routes

--- src/components/UserSignUp.jsx
+++ src/components/UserSignUp.jsx
@@ -6,17 +6,19 @@ import styles from "../App.module.css";
 
 export const UserSignUp = () => {
   return (
-    <div className={styles.sign_up}>
-      <div className={styles.nav}>
-        <div className={styles.user}>
-          <h1 className>User</h1>
+    <div className={styles.container}>
+      <div className={styles.sign_up}>
+        <div className={styles.nav}>
+          <div className={styles.user}>
+            <h1 className>User</h1>
+          </div>
         </div>
-      </div>
-      <div className={styles.user}>
-        <Link to='../homepage' className={styles.back_button}>
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </Link>
-        <h1>Back</h1>
+        <div className={styles.user}>
+          <Link to='../homepage' className={styles.back_button}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
+          <h1>Back</h1>
+        </div>
       </div>
     </div>
   );
