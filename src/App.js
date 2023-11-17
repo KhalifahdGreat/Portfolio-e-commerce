8af39ba109b68af71b52cb769890a8cc718c5c92
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-11-17 16:56:44 +0100

    admin added to route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import styles from "./App.module.css";
 import { UserSignUp } from "./components/UserSignUp";
 import { HomePage } from "./components/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { adminSignUp } from "./components/adminSignUp";
+import AdminSignUp from "./components/adminSignUp";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/signup' element={<UserSignUp />} />
         <Route path='/homepage' element={<HomePage />} />
-        <Route path='/admin' element={<adminSignUp />} />
+        <Route path='/admin' element={<AdminSignUp />} />
         <Route path='*' element={<h1>Error 404</h1>} />
       </Routes>
     </Router>
