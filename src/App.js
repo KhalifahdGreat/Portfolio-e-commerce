ad1456dff49faa49569e6fd1c472e089429fe9a7
muhammed <muhammeddo905@gmail.com>
2023-11-17 09:40:39 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
+import AdminSignUp from "./components/adminSignUp";
 
 function App() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <AdminSignUp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/adminSignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./adminSignUp.css";
+
+export default function AdminSignUp() {
+  return (
+    <div className="container">
+      <h1 className="signHeader">Admin SignUp</h1>
+      <span>Email</span>
+      <span>
+        <input name="email" type="text" />
+      </span>
+    </div>
+  );
+}
