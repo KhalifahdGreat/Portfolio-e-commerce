22999e9ddc224bdb8f47470ac1848650339d2c27
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-11-17 15:23:30 +0100

    created components folder

---
+++ src/components/UserSignUp.jsx
@@ -0,0 +1,13 @@
+const UserSignUp = () => {
+  return (
+    <div>
+      <div className={styles.nav}>
+        <div className={styles.user}>
+          <h1 className></h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserSignUp;
