7491d3f1d5f37575300337175a4fd0276903620a
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-11-19 17:26:30 +0100

    shoppingcart contd

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -1,8 +1,10 @@
 import styles from "../App.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Shopping_cart = () => {
   return (
     <div className={styles.shop_cart_wrapper}>
       <div className={styles.shop_cart_title}>Cart</div>
+      <div className={styles.shop_cart_title}></div>
     </div>
   );
 };
